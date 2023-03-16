2eadb3b2c1fed8aa7c4ceb9ad0230ad0658ee209
2002chris <christian.geier02@t-online.de>
2023-03-16 23:37:21 +0100

    fix annotation

--- PE-application/src/main/java/de/dhbw/ase/application/user/UserApplicationService.java
+++ PE-application/src/main/java/de/dhbw/ase/application/user/UserApplicationService.java
@@ -2,9 +2,11 @@ package de.dhbw.ase.application.user;
 
 import de.dhbw.ase.domain.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserApplicationService implements UserApplication {
 
     private final UserApplication userApplication;
