0b07a650d34f83b6a2ba74568c5f07102f8b16d7
2002chris <christian.geier02@t-online.de>
2023-03-16 19:21:41 +0100

    add todo resource mapper

---
+++ PE-adapters/src/main/java/de/dhbw/ase/adapter/todo/TodoResource.java
@@ -0,0 +1,21 @@
+package de.dhbw.ase.adapter.todo;
+
+import de.dhbw.ase.domain.Tag.Tag;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+public class TodoResource {
+
+    private LocalDate untilDate;
+    private String content;
+    private List<Tag> tags;
+
+    public TodoResource(final LocalDate untilDate, final String content, final List<Tag> tags){
+        this.untilDate = untilDate;
+        this.content = content;
+        this.tags = tags;
+    }
+}
---
+++ PE-adapters/src/main/java/de/dhbw/ase/adapter/todo/TodoToTodoResourceMapper.java
@@ -0,0 +1,18 @@
+package de.dhbw.ase.adapter.todo;
+
+import de.dhbw.ase.domain.todo.Todo;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class TodoToTodoResourceMapper implements Function<Todo, TodoResource> {
+    @Override
+    public TodoResource apply(Todo todo) {
+        return map(todo);
+    }
+
+    private TodoResource map(final Todo todo){
+        return new TodoResource(todo.getUntilDate(), todo.getContent(), todo.getTags());
+    }
+}
