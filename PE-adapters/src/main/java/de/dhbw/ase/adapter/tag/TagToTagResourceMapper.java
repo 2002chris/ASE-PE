de3c45d23514db3302e02301efeb364e13653149
2002chris <coding@cgeier.de>
2023-04-19 16:14:04 +0200

    make TagToTagResourceMapper Component -> needed for Autowiring

--- PE-adapters/src/main/java/de/dhbw/ase/adapter/tag/TagToTagResourceMapper.java
+++ PE-adapters/src/main/java/de/dhbw/ase/adapter/tag/TagToTagResourceMapper.java
@@ -1,9 +1,11 @@
 package de.dhbw.ase.adapter.tag;
 
 import de.dhbw.ase.domain.Tag.Tag;
+import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
+@Component
 public class TagToTagResourceMapper implements Function<Tag,TagResource> {
     @Override
     public TagResource apply(Tag tag) {
