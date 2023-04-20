92c0d2c8e061daa131df3bd56db616d5f3cdc055
2002chris <coding@cgeier.de>
2023-04-20 10:19:20 +0200

    add ReminderController

---
+++ PE-plugins/src/main/java/de/dhbw/ase/rest/ReminderController.java
@@ -0,0 +1,30 @@
+package de.dhbw.ase.rest;
+
+import de.dhbw.ase.adapter.reminder.ReminderResource;
+import de.dhbw.ase.adapter.reminder.ReminderToReminderResourceMapper;
+import de.dhbw.ase.application.reminder.ReminderApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path="/api/reminder")
+public class ReminderController {
+
+    private final ReminderApplication reminderApplication;
+    private final ReminderToReminderResourceMapper reminderToReminderResourceMapper;
+
+    public ReminderController(final ReminderApplication reminderApplication, final ReminderToReminderResourceMapper reminderToReminderResourceMapper)
+    {
+        this.reminderApplication = reminderApplication;
+        this.reminderToReminderResourceMapper = reminderToReminderResourceMapper;
+    }
+
+    @GetMapping
+    public List<ReminderResource> getReminders(){
+        return reminderApplication.findAllReminders().stream().map(reminderToReminderResourceMapper).collect(Collectors.toList());
+    }
+}
