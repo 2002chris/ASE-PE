babb231b14a1137405337e73b426736e69ff5248
2002chris <coding@cgeier.de>
2023-04-20 10:19:44 +0200

    add Tag Controller

---
+++ PE-plugins/src/main/java/de/dhbw/ase/rest/TagController.java
@@ -0,0 +1,32 @@
+package de.dhbw.ase.rest;
+
+import de.dhbw.ase.adapter.tag.TagResource;
+import de.dhbw.ase.adapter.tag.TagToTagResourceMapper;
+import de.dhbw.ase.application.tag.TagApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path = "/api/tag")
+public class TagController {
+
+    private final TagApplication tagApplication;
+    private final TagToTagResourceMapper tagToTagResourceMapper;
+
+    @Autowired
+    public TagController(final TagApplication tagApplication, final TagToTagResourceMapper tagToTagResourceMapper)
+    {
+        this.tagApplication = tagApplication;
+        this.tagToTagResourceMapper = tagToTagResourceMapper;
+    }
+
+    @GetMapping
+    public List<TagResource> getTags(){
+        return tagApplication.findAllTags().stream().map(tagToTagResourceMapper).collect(Collectors.toList());
+    }
+}
