61f41a27997aa87b711fa2b85f1a43e82441d91a
Christian Geier <coding@cgeier.de>
2023-05-29 18:20:24 +0200

    disable cors

--- PE-plugins/src/main/java/de/dhbw/ase/plugins/security/WebSecurityConfig.java
+++ PE-plugins/src/main/java/de/dhbw/ase/plugins/security/WebSecurityConfig.java
@@ -1,12 +1,18 @@
 package de.dhbw.ase.plugins.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/auth/*").permitAll().anyRequest().authenticated().and().httpBasic();
+        http.cors().and().csrf().disable();
 //                .authenticationEntryPoint(authenticationEntryPoint)
 
 //        return http.build();
@@ -43,6 +50,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
         return null;
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
